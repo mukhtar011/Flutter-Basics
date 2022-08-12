fe7432804844f77f728f0b5ef96ecf432866d0ad
user-zoobi <mzXLR82AI@gmail.com>
2022-08-12 22:10:30 +0500

    first project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "my flutter app",
+      home: Scaffold(
+        appBar: AppBar(title: Text("My First App Screen"),),
+      body: Material(
+        color: Colors.amber,
+        child:  Center(
+          child: Text(
+            "hello flutter",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.red,fontSize: 50.0),
+          ),
+        ) ,
+      ),
+    )
+  )
+  );
+}
